ad1715cabf0fe8be5c24ed028e5a85ddd5841093
ECalvet <calveteric63@gmail.com>
2026-03-13 16:46:38 +0100

    Update globe.js

--- js/globe.js
+++ js/globe.js
@@ -1,18 +1,22 @@
 // globe.js
+
+// Initialise le globe
 export function initGlobe() {
-    const globe = new Globe()
+    const globe = new Globe()  // Globe est global grâce au <script> dans HTML
         .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')  // optionnel
-        .polygonsData([])   // on remplira plus tard
+        .polygonsData([])       // on remplira plus tard
         .polygonAltitude(0.01)
-        .polygonCapColor(() => 'green')
-        .polygonSideColor(() => 'rgba(0,100,0,0.5)')
-        .polygonStrokeColor(() => 'black')
-    globe(document.getElementById('globeViz'))
+        .polygonCapColor(() => 'green')               // couleur principale des continents
+        .polygonSideColor(() => 'rgba(0,100,0,0.5)') // côtés (petit relief)
+        .polygonStrokeColor(() => 'black')           // contours
+
+    // Le conteneur dans ton HTML s'appelle maintenant #globe
+    globe(document.getElementById('globe'))
+
     return globe
 }
 
-export function updatePolygons(globe,data){
-
-globe.polygonsData(data.features)
-
+// Met à jour les polygones du globe
+export function updatePolygons(globe, data){
+    globe.polygonsData(data.features)
 }
