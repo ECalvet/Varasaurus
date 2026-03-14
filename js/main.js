637de279f5b9cdb5d0aec6f72c1481e617627163
ECalvet <calveteric63@gmail.com>
2026-03-14 09:41:09 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
+
 import { initGlobe, updateTime } from "./globe.js";
 import { loadGeoJSON } from "./geojsonLoader.js";
 import { loadRotations } from "./rotations.js";
@@ -11,11 +13,11 @@ let rotations;
 
 async function init(){
 
-continents = await loadGeoJSON("data/continents.geojson");
-plates = await loadGeoJSON("data/plates.geojson");
-rotations = await loadRotations("data/rotations.rot");
+continents = await loadGeoJSON("../data/continents.geojson");
+plates = await loadGeoJSON("../data/plates.geojson");
+rotations = await loadRotations("../data/rotations.rot");
 
-initGlobe(continents, plates, rotations);
+initGlobe(THREE, continents, plates, rotations);
 
 }
 
